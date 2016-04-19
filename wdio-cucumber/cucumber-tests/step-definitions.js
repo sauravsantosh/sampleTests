6cfe962d544a405adfc2b0ceaf801d2951fbb626
ssantosh <saurav.sntsh@gmail.com>
2016-04-19 13:10:13 +0100

    Create step-definitions.js

---
+++ wdio-cucumber/cucumber-tests/step-definitions.js
@@ -0,0 +1,39 @@
+/**
+ * to run these tests you need install Cucumber.js on your machine
+ * take a look at https://github.com/cucumber/cucumber-js for more informations
+ *
+ * first, install Cucumber.js via NPM
+ * $ npm install -g cucumber
+ *
+ * then go into the cucumber directory and start the tests with
+ * $ cucumber.js
+ */
+
+var assert = require('assert'),
+    tmpResult;
+
+module.exports = function(){
+
+    this.Given(/^I go on the website "([^"]*)"$/, function(url) {
+        browser.url(url);
+    });
+
+    this.Then(/^should the element "([^"]*)" be (\d+)px wide and (\d+)px high$/, function(selector, width, height) {
+        var elemSize = browser.getElementSize(selector);
+        assert.equal(elemSize.width, width, 'width of element is ' + elemSize.width + ' but should be ' + width);
+        assert.equal(elemSize.height, height, 'height of element is ' + elemSize.height + ' but should be ' + height);
+    });
+
+    // this.Then(/^I click on link "([^"]*)"$/, (selector) => {
+    //     browser.click(selector)
+    // })
+
+    this.Then(/^I click on link "([^"]*)"$/, function (selector){
+        browser.click(selector)
+    });
+
+    this.Then(/^should the title of the page be "([^"]*)"$/, (expectedTitle) => {
+        assert.equal(browser.getTitle(), expectedTitle)
+      });
+
+};
