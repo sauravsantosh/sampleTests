71c5ae3bdd7f68a6a2b6221f5703e51f167d1e09
ssantosh <saurav.sntsh@gmail.com>
2016-04-19 13:25:51 +0100

    Update wdio.cucumber.local.js

--- wdio-cucumber/wdio.cucumber.local.js
+++ wdio-cucumber/wdio.cucumber.local.js
@@ -9,14 +9,21 @@ exports.config = {
     /**
      * specify test files
      */
-     maxInstances: 1,
     specs: [
         './cucumber-tests/features/*.feature'
     ],
     exclude: [
 
     ],
-
+    // First, you can define how many instances should be started at the same time. Let's
+    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
+    // set maxInstances to 1, wdio will spawn 3 processes. Therefore, if you have 10 spec
+    // files and you set maxInstances to 5; all spec files will get tested at the same time
+    // and 30 processes will get spawned. The property handles how many capabilities
+    // from the same test should run tests. (v4 features)
+    //
+    //
+    maxInstances: 1,
     /**
      * capabilities
      */
